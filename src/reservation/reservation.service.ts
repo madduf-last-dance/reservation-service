79cd3efee2c448dc207922422d56fe37eee46398
VukoticMarko <vukoticmarko99@Gmail.com>
2024-05-07 16:42:35 +0200

    feat: added check function

--- src/reservation/reservation.service.ts
+++ src/reservation/reservation.service.ts
@@ -23,4 +23,8 @@ export class ReservationService {
   remove(id: number) {
     return `This action removes a #${id} reservation`;
   }
+
+  checkIfTaken(id: number) {
+    return `This action checks if a #${id} reservation was made`;
+  }
 }
