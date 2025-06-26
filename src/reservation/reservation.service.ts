1051145896e005e1eeebb9b16cc54b4730f93307
Nikola Petrovic <40899687+nikolapetrovic1@users.noreply.github.com>
2025-06-26 15:16:39 +0200

    Feat/get user and accepted reservations (#11)
    * feat: added method for fetching all user pending and all accepted reservations for accommodation
    
    * feat: accept reservation
    
    * feat: update reseration check

--- src/reservation/reservation.service.ts
+++ src/reservation/reservation.service.ts
@@ -19,7 +19,7 @@ export class ReservationService {
 
   async create(reservationDto: ReservationDto): Promise<Reservation> {
     reservationDto.status = Status.PENDING;
-    const isOk = this.checkReservation(reservationDto);
+    const isOk = await this.checkReservation(reservationDto);
     if (!isOk) {
       return null;
     }
@@ -171,7 +171,6 @@ export class ReservationService {
     const accommodation = await lastValueFrom(this.accommodationClient.send<any>("findOneAccommodation", dto.accommodationId));
     if (!accommodation) {
       return false;
-      // throw new NotFoundException(`Accommodation with ID ${dto.accommodationId} not found`);
     }
     const reservations = await this.reservationRepository.find({
       where: {
@@ -189,11 +188,37 @@ export class ReservationService {
       startDate: dto.startDate,
       endDate: dto.endDate,
     };
-    const bool = await this.accommodationClient
+    const hasAvailability = await this.accommodationClient
       .send<any>("checkAvailability", aDto).toPromise();
+    console.log(hasAvailability + " _ hasAvailability");
 
-    if (!bool) {
-      return false;
+    return hasAvailability;
+  }
+
+  async findAllGuestAndAccepted(guestId: number, accommodationId: number) {
+    const accommodation = await lastValueFrom(this.accommodationClient.send<any>("findOneAccommodation", accommodationId));
+    if (!accommodation) {
+      return null;
+    }
+    const guestPendingReservations = await this.reservationRepository.find(
+      { where: { guestId: guestId, accommodationId: accommodationId, status: Status.PENDING } }
+    );
+    const acceptedReservations = await this.reservationRepository.find(
+      { where: { accommodationId: accommodationId, status: Status.ACCEPTED } }
+    );
+    return [...acceptedReservations, ...guestPendingReservations];
+  }
+
+  async acceptReservation(reservationId: number) {
+    const reservation = await this.reservationRepository.findOne({
+      where: {
+        id: reservationId,
+        status: Status.PENDING,
+      },
+    });
+    if (reservation) {
+      reservation.status = Status.ACCEPTED;
+      return await this.reservationRepository.save(reservation);
     }
   }
 
